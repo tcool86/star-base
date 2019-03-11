b583e0ae7a5851d4b5745f3eb5056299bd45265a
Timothy Cool <tcoolpg@gmail.com>
2019-03-10 23:15:24 -0700

    split dev/prod webpack config, express boilerplate

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 var phaser = path.join(pathToPhaser, 'dist/phaser.js');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 module.exports = {
   entry: './src/boilerplate/game.ts',
@@ -14,13 +14,6 @@ module.exports = {
       { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
       { test: /phaser\.js$/, loader: 'expose-loader?Phaser' }
     ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './'),
-    publicPath: '/dist/',
-    host: '127.0.0.1',
-    port: port,
-    open: true
   },
   resolve: {
     extensions: ['.ts', '.js'],
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+var path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+    publicPath: '/dist/',
+    host: '127.0.0.1',
+    port: 8080,
+    open: true
+  }
+});
