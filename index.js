090f4cca19cb1ad2a808feea43d23cbf88ab7671
Timothy Cool <tcoolpg@gmail.com>
2019-03-11 21:55:13 -0700

    open request to other file types

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/styles'));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
